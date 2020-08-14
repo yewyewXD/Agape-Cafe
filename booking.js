91a18cf13e1b3a56e9f216b6154c089bdfc70980
Yew Kang Wei <yewyew6933@gmail.com>
2020-08-14 13:22:32 +0800

    encode and validate init

--- booking.js
+++ booking.js
@@ -1,12 +1,15 @@
-const emailInput = document.getElementById("emailInput");
-const dateInput = document.getElementById("dateInput");
-const timeInput = document.getElementById("timeInput");
-const partyInput = document.getElementById("partyInput");
+function submitBooking() {
+  const nameInput = document.getElementById("nameInput").value;
+  const dateInput = document.getElementById("dateInput").value;
+  const timeInput = document.getElementById("timeInput").value;
+  const partyInput = document.getElementById("partyInput").value;
 
-function submitBooking(e) {
-  e.preventDefault();
-  console.log(emailInput.value);
-  console.log(dateInput.value);
-  console.log(timeInput.value);
-  console.log(partyInput.value);
+  const waURL = `https://wa.me/60167212991?text=Hi%21+I+came+across+your+ASAP+website+and+would+like+to+make+a+booking%21%0D%0AName%3A+${nameInput}%0D%0ADate%3A+${dateInput}%0D%0ATime%3A+${timeInput}%0D%0AParty+Size%3A+${partyInput}`;
+
+  //validation
+  if (nameInput.trim().length < 1 || dateInput.trim().length < 1) {
+    console.log("validate warning");
+  } else {
+    window.location.href = waURL;
+  }
 }
