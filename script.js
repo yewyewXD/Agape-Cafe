1f5e91c1eb2d9d36672891ee79b0972a2bb8201b
Yew Kang Wei <yewyew6933@gmail.com>
2020-09-09 13:16:01 +0800

    fix booking form

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+// handle booking submit
+const bookForm = document.getElementById("booking-form");
+
+bookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  submitBooking();
+});
+
 function submitBooking() {
   const nameInput = document.getElementById("nameInput").value;
   const dateInput = document.getElementById("dateInput").value;
   const timeInput = document.getElementById("timeInput").value;
   const partyInput = document.getElementById("partyInput").value;
 
-  const waURL = `https://wa.me/60167212991?text=Hi%21+I+came+across+your+ASAP+website+and+would+like+to+make+a+booking%21%0D%0AName%3A+${nameInput}%0D%0ADate%3A+${dateInput}%0D%0ATime%3A+${timeInput}%0D%0AParty+Size%3A+${partyInput}`;
+  const waURL = `https://wa.me/60167212991?text=Hi%21+I+came+across+your+ASAP+website+and+would+like+to+make+a+booking%21%0D%0AName%3A+${nameInput}%0D%0ADate%3A+${dateInput}%0D%0ATime%3A+${timeInput}%0D%0AGroup+Size%3A+${partyInput}`;
 
   //validation
   if (nameInput.trim().length < 1 || dateInput.trim().length < 1) {
@@ -14,6 +22,7 @@ function submitBooking() {
   }
 }
 
+// handle navbar onscroll behavior
 const navbar = document.getElementById("navbar");
 const lightLogo = document.getElementsByClassName("logo-text-white")[0];
 const darkLogo = document.getElementsByClassName("logo-text-dark")[0];
